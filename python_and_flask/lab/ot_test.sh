aab2339d24496e321d1218140255b174b264889a
Oytun Turk <oytunturk@gmail.com>
2023-10-23 19:40:39 -0700

    Updates

--- python_and_flask/lab/ot_test.sh
+++ python_and_flask/lab/ot_test.sh
@@ -1,3 +1,18 @@
 flask --app server --debug run
+
 curl -X GET -i -w '\n' localhost:5000
+
 curl -X GET -i -w '\n' localhost:5000/no_content
+
+curl -X GET -i -w '\n' localhost:5000/exp
+
+curl -X GET -i -w '\n' localhost:5000/data
+
+#Existing person
+curl -X GET -i -w '\n' "localhost:5000/name_search?q=Abdel"
+
+#Missing q
+curl -X GET -i -w '\n' "localhost:5000/name_search"
+
+#Non-existing person
+curl -X GET -i -w '\n' "localhost:5000/name_search?q=qwerty"
--- python_and_flask/lab/server.py
+++ python_and_flask/lab/server.py
@@ -1,13 +1,77 @@
 from flask import Flask, make_response
 app = Flask(__name__)
 
+data = [
+    {
+        "id": "3b58aade-8415-49dd-88db-8d7bce14932a",
+        "first_name": "Tanya",
+        "last_name": "Slad",
+        "graduation_year": 1996,
+        "address": "043 Heath Hill",
+        "city": "Dayton",
+        "zip": "45426",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/139x100.png/cc0000/ffffff",
+    },
+    {
+        "id": "d64efd92-ca8e-40da-b234-47e6403eb167",
+        "first_name": "Ferdy",
+        "last_name": "Garrow",
+        "graduation_year": 1970,
+        "address": "10 Wayridge Terrace",
+        "city": "North Little Rock",
+        "zip": "72199",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/148x100.png/dddddd/000000",
+    },
+    {
+        "id": "66c09925-589a-43b6-9a5d-d1601cf53287",
+        "first_name": "Lilla",
+        "last_name": "Aupol",
+        "graduation_year": 1985,
+        "address": "637 Carey Pass",
+        "city": "Gainesville",
+        "zip": "32627",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/174x100.png/ff4444/ffffff",
+    },
+    {
+        "id": "0dd63e57-0b5f-44bc-94ae-5c1b4947cb49",
+        "first_name": "Abdel",
+        "last_name": "Duke",
+        "graduation_year": 1995,
+        "address": "2 Lake View Point",
+        "city": "Shreveport",
+        "zip": "71105",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/145x100.png/dddddd/000000",
+    },
+    {
+        "id": "a3d8adba-4c20-495f-b4c4-f7de8b9cfb15",
+        "first_name": "Corby",
+        "last_name": "Tettley",
+        "graduation_year": 1984,
+        "address": "90329 Amoth Drive",
+        "city": "Boulder",
+        "zip": "80305",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/198x100.png/cc0000/ffffff",
+    }
+]
+
 @app.route("/")
 def index():
     return "hello world"
 
 @app.route("/no_content")
-def content_error():
-    return ({"message":"No content found"},{"status_code":204})
+def no_content():
+    """return 'No content found' with a status of 204
+
+    Returns:
+        string: No content found
+        status code: 204
+    """
+    return ({"message":"Hello World"}, 204)
 
 @app.route("/exp")
 def index_explicit():
@@ -17,6 +81,38 @@ def index_explicit():
         string: Hello World
         status code: 200
     """
-    resp = make_response({"message": "Hello World"})
+    resp = make_response({"message":"Hello World"})
     resp.status_code = 200
     return resp
+
+@app.route("/data")
+def get_data():
+    try:
+        if data and len(data) > 0:
+            return {"message": f"Data of length {len(data)} found"}
+        else:
+            return {"message": "Data is empty"}, 500
+    except NameError:
+        return {"message": "Data not found"}, 404
+
+@app.route("/name_search")
+def name_search():
+    """find a person in the database
+
+    Returns:
+        json: person if found, with status of 200
+        404: if not found
+        422: if argument q is missing
+    """
+    query = request.args.get("q")
+    person_from_query = query.lower()
+
+    if not query:
+        return {"message": "Error! q is missing"}, 422
+
+    # this code goes through data and looks for the first_name
+    for person in data:
+        if person_from_query in person["first_name"].lower():
+            return {"message": person}, 200
+
+    return {"message": f'Error! {person_from_query} not found'}, 404
