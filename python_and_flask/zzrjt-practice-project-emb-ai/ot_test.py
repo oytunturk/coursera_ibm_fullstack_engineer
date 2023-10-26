b2fd1b23d5cd2f368e635899971bde5c76aced2a
Oytun Turk <oytunturk@gmail.com>
2023-10-26 09:27:37 -0700

    Call to Watson

--- python_and_flask/zzrjt-practice-project-emb-ai/ot_test.py
+++ python_and_flask/zzrjt-practice-project-emb-ai/ot_test.py
@@ -1,7 +1,21 @@
-from sentiment_analysis import sentiment_analyzer
+from sentiment_analysis import sentiment_analyzer_basic, sentiment_analyzer
 import json
 
-response = sentiment_analyzer("I love this new technology")
+text = "I love this new technology"
+
+#Basic function that doesn't parse anything
+print("--> Calling sentiment_analyzer_basic():")
+response = sentiment_analyzer_basic(text)
 formatted_response = json.loads(response)
 print(formatted_response)
 
+label = formatted_response['documentSentiment']['label']
+score = formatted_response['documentSentiment']['score']
+
+print('label = ' + str(label))
+print('score = ' + str(score))
+
+#Function that parses and returns label and score
+print("--> Calling sentiment_analyzer():")
+result = sentiment_analyzer(text)
+print(str(result))
