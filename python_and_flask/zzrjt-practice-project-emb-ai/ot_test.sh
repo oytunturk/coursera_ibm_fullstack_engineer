fea3d4315dfa5286a26763455b6ebb7298b08080
Oytun Turk <oytunturk@gmail.com>
2023-10-26 10:07:01 -0700

    Simple web server using Flask

--- python_and_flask/zzrjt-practice-project-emb-ai/ot_test.sh
+++ python_and_flask/zzrjt-practice-project-emb-ai/ot_test.sh
@@ -3,3 +3,8 @@ python3 ot_test.py
 
 #This will run unit tests I created
 python3 test_sentiment_analysis.py
+
+#This will run the web server implemented using Flask and the templates provided in the project (html and javascript)
+python3 server.py
+
+#After running the server, go to localhost:5000 in your browser to test it
--- python_and_flask/zzrjt-practice-project-emb-ai/server.py
+++ python_and_flask/zzrjt-practice-project-emb-ai/server.py
@@ -2,10 +2,13 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
+# Import Flask, render_template, request from the flask framework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 #Initiate the flask app : TODO
+app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
@@ -14,15 +17,19 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    result = sentiment_analyzer(text_to_analyze)
+    label, score = result['label'], result['score']
+    label = label.split('_')[1]
+    return f'The given text has been identified as {label} with a score of {score}', 200
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    # Run the app
+    app.run(host="0.0.0.0", port=5000)
