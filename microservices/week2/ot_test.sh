d007776a16bc0ab9e5094e6baf76717281dba171
oytunturk <oytunturk@github.com>
2023-11-22 11:37:42 -0800

    microservices

---
+++ microservices/week2/ot_test.sh
@@ -0,0 +1,8 @@
+#Install Flask:
+python3 -m pip install flask
+
+#Run the server
+python3 server.py
+
+#Go to http://127.0.0.0:5000 on your browser to see what it does or curl it
+curl http://127.0.0.1:5000
